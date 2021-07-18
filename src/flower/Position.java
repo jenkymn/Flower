6e9c8385c43a71709b6fb3b86ad13d0eb5dc86ea
jenkymn <87531839+jenkymn@users.noreply.github.com>
2021-07-18 13:53:05 +0800

    绘制中心的坐标

---
+++ src/flower/Position.java
@@ -0,0 +1,20 @@
+package demo.flower;
+
+public class Position {
+    private final double x;
+    private final double y;
+
+    public Position(double xpos, double ypos) {
+        this.x = xpos;
+        this.y = ypos;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+}
+
